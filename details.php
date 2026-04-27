29378104fd89a61c9028d44c9d8dfaaad9d35b30
git-princess <panganibanprincessnicole03@gmail.com>
2026-04-28 07:02:40 +0800

    line 141&144 reviews(allReviews)

--- details.php
+++ details.php
@@ -138,10 +138,10 @@ $avgRating = getAttractionRating($id);
     </form>
   </div>
 
-  <?php if($allReviews): ?>
+  <?php if($reviews): ?>
     <div class="detail-content">
       <h3 style="margin-bottom:16px;">Reviews (<?= count($allReviews) ?>)</h3>
-      <?php foreach($allReviews as $r): ?>
+      <?php foreach($reviews as $r): ?>
         <div class="review-card">
           <div class="rev-header">
             <span class="rev-user"><?= htmlspecialchars($r['user']) ?></span>

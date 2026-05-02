b64e060f19c6eb944ea41c81f4c2a712dc9c04c4
Aimee Ocampo <ocampoaimee12@gmail.com>
2026-05-02 16:03:14 +0800

    Yun

--- api/server.py
+++ api/server.py
@@ -69,5 +69,5 @@ def add_cors_headers(response):
 
 if __name__ == '__main__':
     host = os.environ.get('FLASK_HOST', '0.0.0.0')
-    port = int(os.environ.get('FLASK_PORT', 5000))
+    port = int(os.environ.get('PORT', os.environ.get('FLASK_PORT', 5000)))
     app.run(host=host, port=port)
